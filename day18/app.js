8b5cf0e6ba1c7ff0eb29bcda4f13264a748bf371
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-07-28 13:03:29 +0530

    day18

---
+++ day18/app.js
@@ -0,0 +1,10 @@
+var strStr = function(haystack, needle) {
+    // let 
+    // for(let i = 0;i<haystack.length;i++) {
+
+    // }
+    // return haystack.indexOf(needle);
+};
+console.log(strStr("sadbutsad","sad"));
+console.log(strStr("leetcode","leeto"));
+console.log(strStr("leetcodeleeto","leeto"));
